13919847c2357146e8bef4d4ce49985a9ff4b323
Leviaweek <113444876+Leviaweek@users.noreply.github.com>
2022-12-23 14:21:03 +0200

    Update questions.py

--- arithmetic_test/questions.py
+++ arithmetic_test/questions.py
@@ -4,7 +4,7 @@ from test import Test
 class Easy(Test):
     """Class for easy level test"""
     def __init__(self):
-        self.operations = ["+", "-"]
+        self.operations = ("+", "-")
         self.rand_min = 1
         self.rand_max = 10
         self.name = "easy"
@@ -13,7 +13,7 @@ class Easy(Test):
 class Medium(Test):
     """Class for meidum level test"""
     def __init__(self):
-        self.operations = ["+", "-", "*", "/"]
+        self.operations = ("+", "-", "*", "/")
         self.rand_min = -100
         self.rand_max = 100
         self.name = "normal"
@@ -22,7 +22,7 @@ class Medium(Test):
 class Hard(Test):
     """Class for hard level test"""
     def __init__(self):
-        self.operations = ["+", "-", "*", "/"]
+        self.operations = ("+", "-", "*", "/")
         self.rand_min = -1000
         self.rand_max = 1000
         self.name = "hard"
@@ -31,7 +31,7 @@ class Hard(Test):
 class Unreal(Test):
     """Class for unreal level test"""
     def __init__(self):
-        self.operations = ["**"]
+        self.operations = ("**")
         self.rand_min = 2
         self.rand_max = 10
         self.name = "unreal"
